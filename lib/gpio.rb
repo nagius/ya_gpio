38e5ea886b41a5586817d71a556a783397887b55
Nicolas AGIUS <nicolas.agius@lps-it.fr>
2019-08-27 19:39:44 +0100

    First commit

---
+++ lib/gpio.rb
@@ -0,0 +1,171 @@
+#!/usr/bin/env ruby
+
+
+# Please note that lib require read/write access to the GPIO Sysfs subdirectory
+# if your script is not running as root, add your user to the gpio group
+
+# Use sysfs interface so do not support integrated pull-down an dull-up resistors
+# https://www.kernel.org/doc/Documentation/gpio/sysfs.txt
+
+
+
+class GPIO
+	attr_reader :file	
+
+	INPUT = "in"
+	OUTPUT = "out"
+	OUTPUT_HIGH = "high"
+
+	EDGE_RISING = 'rising'
+	EDGE_FALLING = 'falling'
+	EDGE_BOTH = 'both'
+	EDGE_NONE = 'none'
+
+	MAX_RETRY = 3
+
+	@@wait = true
+
+	def initialize(pin, direction)
+		raise 'direction must be one of INPUT, OUTPUT, OUTPUT_HIGH' unless [INPUT, OUTPUT, OUTPUT_HIGH].include?(direction)
+
+		@pin = pin
+		@callback = nil
+		@direction = direction
+
+		export
+		open
+
+		ObjectSpace.define_finalizer(self, self.class.finalize(@pin))
+	end
+
+	# Unexport the port when the program exits
+	def self.finalize(pin)
+		proc do
+			File.write('/sys/class/gpio/unexport', pin)
+ 		end
+	end
+
+	def high?
+		read() != 0
+	end
+
+	def low?
+		read() == 0
+	end
+
+	def high
+		write(1)
+	end
+
+	def low
+		write(0)
+	end
+
+	# Invert all values and settings. After set to true, HIGH means LOW and LOW means HIGH.
+	# Be prepared to be confused if you're using this feature
+	def active_low=(active)
+		File.write("/sys/class/gpio/gpio#{@pin}/active_low", active ? '1' : '0')
+	end
+
+	def active_low?
+		File.read("/sys/class/gpio/gpio#{@pin}/active_low") != '0'
+	end
+
+	def set_interrupt(edge, &block)
+		raise 'interrupt can only be set on input pin' unless @direction == INPUT
+
+		set_edge(edge)	
+		@callback = block
+	end
+
+	def clear_interrupt()
+		set_edge(EDGE_NONE)	
+		@callback = nil
+	end
+
+	def trigger(active)
+		if @callback.nil?
+		    puts "No Callback defined for #{@pin}"
+		else
+		    @callback.call(active)
+		end
+	end
+
+	def unexport()
+		File.write('/sys/class/gpio/unexport', @pin.to_s)
+	end
+
+	# Software debounce has not been implemented.
+	# You can use a 1uF capacitor in your setup to fix bounce issues.
+	#
+	# Will block until the program exits or GPIO::resume() is called from a callback
+	def self.wait(gpios)
+		# Initial read to clear interrupt triggered during setup 
+		gpios.map{|g| g.high?}
+
+		@@wait = true
+		while @@wait do
+			rs, ws, es = IO.select(nil, nil, gpios.map{|g| g.file})
+
+			es.each do |f|
+				gpio = gpios.select{|g| g.file == f}.first
+				gpio.trigger(gpio.high?)
+			end
+		end
+	end
+
+	# Stop the wait loop, must be run from a callbacktriggered by the wait
+	def self.resume
+		@@wait = false
+	end
+
+	private
+
+	def export()
+		begin
+			File.write('/sys/class/gpio/export', @pin.to_s)
+		rescue Errno::EBUSY
+			puts "WARNING: GPIO #{@pin} is already exported, may be in use."
+		end
+	end
+
+	def open
+		begin
+			retries ||= 0
+			case @direction
+				when INPUT
+					@file=File.open("/sys/class/gpio/gpio#{@pin}/value", "r")
+				else
+					@file=File.open("/sys/class/gpio/gpio#{@pin}/value", "r+")
+			end
+
+			@file.sync=true	# Auto flush
+			File.write("/sys/class/gpio/gpio#{@pin}/direction", @direction)
+		rescue Errno::EACCES
+			if (retries += 1) <= MAX_RETRY
+				puts "Permission on sysfs not ready yet, retrying (#{retries}/#{MAX_RETRY})..."
+				sleep 0.1
+				retry
+			end
+
+			raise
+		end
+	end
+
+	def set_edge(edge)
+		raise 'edge must be one of EDGE_RISING, EDGE_FALLING, EDGE_BOTH, EDGE_NONE' unless [EDGE_RISING, EDGE_FALLING, EDGE_BOTH, EDGE_NONE].include?(edge)
+
+		File.write("/sys/class/gpio/gpio#{@pin}/edge", edge)
+	end
+
+	def read()
+		@file.seek(0, IO::SEEK_SET)
+		@file.read().to_i
+	end
+
+	def write(value)
+		@file.seek(0, IO::SEEK_SET)
+		@file.write(value)
+	end
+end
+
---
+++ spec/gpio_spec.rb
@@ -0,0 +1,152 @@
+require_relative 'spec_helper'
+
+# TODO ecelption interruption sur output
+# test permission?
+
+
+describe GPIO do
+	include FakeFS::SpecHelpers
+
+	let(:pin) { 12 }
+
+	before do
+		FileUtils.makedirs("/sys/class/gpio/gpio#{pin}")
+		FileUtils.touch("/sys/class/gpio/gpio#{pin}/value")
+	end
+
+	it 'open a GPIO as output' do
+		gpio = GPIO.new(pin, GPIO::OUTPUT)
+
+		expect(File.read('/sys/class/gpio/export')).to eq pin.to_s
+		expect(File.read("/sys/class/gpio/gpio#{pin}/direction")).to eq 'out'
+	end
+
+	it 'open a GPIO as output with default HIGH value' do
+		gpio = GPIO.new(pin, GPIO::OUTPUT_HIGH)
+
+		expect(File.read('/sys/class/gpio/export')).to eq pin.to_s
+		expect(File.read("/sys/class/gpio/gpio#{pin}/direction")).to eq 'high'
+	end
+
+	it 'open a GPIO as input' do
+		gpio = GPIO.new(pin, GPIO::INPUT)
+
+		expect(File.read('/sys/class/gpio/export')).to eq pin.to_s
+		expect(File.read("/sys/class/gpio/gpio#{pin}/direction")).to eq 'in'
+	end
+
+	it 'close the GPIO' do
+		gpio = GPIO.new(pin, GPIO::INPUT)
+		gpio.unexport
+
+		expect(File.read('/sys/class/gpio/unexport')).to eq pin.to_s
+	end
+		
+	it 'close the GPIO when destroyed' do
+		gpio = GPIO.new(pin, GPIO::INPUT)
+		gpio = nil
+
+		GC.start
+		expect(File.read('/sys/class/gpio/unexport')).to eq pin.to_s
+	end
+
+	it 'raise when instantiated with wrong direction' do
+		expect{GPIO.new(pin, 'wrong-direction')}.to raise_error(RuntimeError)
+	end
+
+	it 'set active_low' do
+		gpio = GPIO.new(pin, GPIO::OUTPUT)
+
+		gpio.active_low = true
+		expect(File.read("/sys/class/gpio/gpio#{pin}/active_low")).to eq '1'
+
+		gpio.active_low = false
+		expect(File.read("/sys/class/gpio/gpio#{pin}/active_low")).to eq '0'
+	end
+		
+	it 'get active_low' do
+		gpio = GPIO.new(pin, GPIO::OUTPUT)
+
+		File.write("/sys/class/gpio/gpio#{pin}/active_low", '1')
+		expect(gpio.active_low?).to be true
+
+		File.write("/sys/class/gpio/gpio#{pin}/active_low", '0')
+		expect(gpio.active_low?).to be false
+	end
+			
+		
+	it 'reads high value' do
+		File.write("/sys/class/gpio/gpio#{pin}/value", '1')
+		gpio = GPIO.new(pin, GPIO::INPUT)
+
+		expect(gpio.high?).to be true
+		expect(gpio.low?).to be false
+	end
+
+	it 'reads low value' do
+		File.write("/sys/class/gpio/gpio#{pin}/value", '0')
+		gpio = GPIO.new(pin, GPIO::INPUT)
+
+		expect(gpio.high?).to be false
+		expect(gpio.low?).to be true
+	end
+
+	it 'set high value' do
+		gpio = GPIO.new(pin, GPIO::OUTPUT)
+		gpio.high
+
+		expect(File.read("/sys/class/gpio/gpio#{pin}/value")).to eq '1'
+	end
+		
+	it 'set low value' do
+		gpio = GPIO.new(pin, GPIO::OUTPUT)
+		gpio.low
+
+		expect(File.read("/sys/class/gpio/gpio#{pin}/value")).to eq '0'
+	end
+
+	it 'setup interrupt with edge' do
+		gpio = GPIO.new(pin, GPIO::INPUT)
+
+		gpio.set_interrupt(GPIO::EDGE_RISING) 
+		expect(File.read("/sys/class/gpio/gpio#{pin}/edge")).to eq 'rising'
+
+		gpio.set_interrupt(GPIO::EDGE_FALLING) 
+		expect(File.read("/sys/class/gpio/gpio#{pin}/edge")).to eq 'falling'
+
+		gpio.set_interrupt(GPIO::EDGE_BOTH) 
+		expect(File.read("/sys/class/gpio/gpio#{pin}/edge")).to eq 'both'
+	end
+
+	it 'raise when interrupt set with wrong edge' do
+		gpio = GPIO.new(pin, GPIO::INPUT)
+
+		expect{gpio.set_interrupt('wrong-edge')}.to raise_error(RuntimeError)
+	end
+
+	it 'clear interrupt' do
+		gpio = GPIO.new(pin, GPIO::INPUT)
+		gpio.clear_interrupt
+
+		expect(File.read("/sys/class/gpio/gpio#{pin}/edge")).to eq 'none'
+	end
+
+	it 'wait for an interrupt and run callback once' do
+		callback_count = 0
+		File.write("/sys/class/gpio/gpio#{pin}/value", '1')
+
+		gpio = GPIO.new(pin, GPIO::INPUT)
+		gpio.set_interrupt(GPIO::EDGE_BOTH) do |active|
+			callback_count += 1
+			expect(active).to be true
+			
+			GPIO::resume # Prevent infinite loop during testing
+		end
+
+		allow(IO).to receive(:select).with(nil, nil, [gpio.file]).and_return([nil, nil, [gpio.file]])
+		GPIO::wait([gpio])
+		expect(callback_count).to eq 1
+	end
+	
+end
+
